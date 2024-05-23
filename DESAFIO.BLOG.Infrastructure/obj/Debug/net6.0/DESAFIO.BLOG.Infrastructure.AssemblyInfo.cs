781fa0a65b11d4b35b26b080d930e44ac6a7811f
Paulo Junior <pseaj@hotmail.com>
2024-05-23 12:39:44 -0300

    GIT para o Front VSCode

--- DESAFIO.BLOG.Infrastructure/obj/Debug/net6.0/DESAFIO.BLOG.Infrastructure.AssemblyInfo.cs
+++ DESAFIO.BLOG.Infrastructure/obj/Debug/net6.0/DESAFIO.BLOG.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DESAFIO.BLOG.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ba9510e85af8e3eb96518e5fe335158b52df18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea3ca68724ed9df85089cc685c8088fdd083cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DESAFIO.BLOG.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DESAFIO.BLOG.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
